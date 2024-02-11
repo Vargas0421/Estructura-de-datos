a3f08103007291e0457910da1ed0cd706245d3af
Vargas0421 <136260759+Vargas0421@users.noreply.github.com>
2024-02-11 15:08:11 -0600

    Add files via upload

--- 402640854_PP2/src/pkg402640854_pp2/Main.java
+++ 402640854_PP2/src/pkg402640854_pp2/Main.java
@@ -1,14 +1,13 @@
 package pkg402640854_pp2;
-
-import javax.swing.JOptionPane;
-
 public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
 
-        Ejecucion ejecu = new Ejecucion();
-        ejecu.ejecucion();
+        //Ejecucion ejecu = new Ejecucion();
+       // ejecu.ejecucion();
+        //Ejecucion_2 ejecu2 = new Ejecucion_2();
+        //ejecu2.Ejecucion2();
                 
 
     }
